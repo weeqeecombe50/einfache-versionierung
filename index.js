c8ec24c8ce197b2d8111b729f15cd07de5585e84
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-25 17:51:52 +0300

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Willkommen bei Einfache Versionierung!');
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft unter http://localhost:${port}`);
+});
